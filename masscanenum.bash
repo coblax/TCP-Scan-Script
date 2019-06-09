14a654f1a866bf06a16fc527e5693e86e4197430
coblax <51140182+coblax@users.noreply.github.com>
2019-06-09 18:51:44 +0700

    Update masscanenum.bash

--- masscanenum.bash
+++ masscanenum.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
-ip="10.10.10.95" #change your IP
+echo -n "Ip address = "
+read ip
+echo -n "Name File = "
+read Name
 masscan -p1-65535 $ip --rate=1000 -e tun0 > ports.txt
 ports=$(cat ports.txt | awk -F " " '{print $4}' | awk -F "/" '{print $1}' | sort -n | tr '\n' ',' | sed 's/,$//');
-nmap -sC -sV -p$ports $ip
+nmap -sC -sV -p$ports $ip -oN $Name.nmap
